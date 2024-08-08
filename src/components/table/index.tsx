a8550f8b7492b869e10acb3cbeca90fc62c24e7e
Abdisa Dev <AbdisaDev@gmail.com>
2024-08-08 16:55:37 +0300

    data grid

---
+++ src/components/table/index.tsx
@@ -0,0 +1,102 @@
+import {
+  DataGrid,
+  GridRowsProp,
+  GridColDef,
+  GridToolbar,
+  GridToolbarContainer,
+  GridToolbarQuickFilter,
+} from '@mui/x-data-grid';
+import { Box, Typography, styled } from '@mui/material';
+
+const Toolbar = styled(GridToolbar)(() => ({
+  // Your custom styles here to hide labels
+  '& .MuiDataGrid-toolbarContainer .MuiButtonBase-root': {
+    '& .MuiButton-label': {
+      display: 'none',
+    },
+  },
+}));
+
+function SearchToolBar(props: { title: string }) {
+  return (
+    <GridToolbarContainer
+      sx={{
+        justifyContent: 'space-between',
+        my: '10px',
+        mx: '30px',
+      }}
+    >
+      <Box>
+        <Typography variant='h6' fontWeight='700'>
+          {props.title}
+        </Typography>
+      </Box>
+      <Box sx={{ display: 'flex', columnGap: 2 }}>
+        <Box>
+          <GridToolbarQuickFilter variant='standard' />
+        </Box>
+        <Box>
+          <Toolbar />
+        </Box>
+      </Box>
+    </GridToolbarContainer>
+  );
+}
+
+function Table(props: {
+  rows: GridRowsProp;
+  columns: GridColDef[];
+  isLoading: boolean;
+  errorMessage?: string | undefined;
+  title: string;
+}) {
+  return (
+    <Box>
+      <DataGrid
+        sx={{
+          boxShadow: '0 0 20px #E1D9D1',
+          backgroundColor: 'white',
+          borderRadius: '15px',
+          px: 2,
+        }}
+        rows={props.rows}
+        columns={props.columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 6,
+            },
+          },
+          filter: {
+            filterModel: {
+              items: [],
+              quickFilterExcludeHiddenColumns: true,
+            },
+          },
+        }}
+        autoHeight
+        loading={props.isLoading}
+        slotProps={{
+          loadingOverlay: {
+            variant: 'skeleton',
+            noRowsVariant: 'skeleton',
+          },
+          toolbar: {
+            showQuickFilter: true,
+          },
+        }}
+        slots={{
+          toolbar: () => (
+            <Box>
+              <SearchToolBar title={props.title} />
+            </Box>
+          ),
+        }}
+        pageSizeOptions={[13]}
+        localeText={{ noRowsLabel: props.errorMessage }}
+      />
+    </Box>
+  );
+}
+
+export default Table;
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,11 +1,61 @@
 import { Box } from '@mui/material';
 import Statistics from '../../components/statistics';
+import Table from '../../components/table';
+import { GridColDef } from '@mui/x-data-grid';
 
+const columns: GridColDef<(typeof rows)[number]>[] = [
+  { field: 'id', headerName: 'ID', width: 90 },
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    flex: 1,
+  },
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    flex: 1,
+  },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    flex: 1,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    flex: 1,
+    valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+  },
+];
+
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
 function AdminDashBoard() {
   return (
-    <Box sx={{ height: '87vh' }}>
+    <Box
+      sx={{ height: '87vh', display: 'flex', justifyContent: 'space-between' }}
+    >
       <Box sx={{ height: '100%' }}>
         <Statistics />
+      </Box>
+      <Box sx={{ width: '90%', ml: '20px', mt: '20px' }}>
+        <Table
+          rows={rows}
+          columns={columns}
+          isLoading={false}
+          title='Live Book Status'
+        />
       </Box>
     </Box>
   );
