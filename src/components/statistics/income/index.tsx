4188a69649dc03834ba4df28444c01f9a8ee7fed
Abdisa Dev <abdisadev@gmail.com>
2024-08-13 22:14:21 +0300

    font

--- src/components/statistics/income/index.tsx
+++ src/components/statistics/income/index.tsx
@@ -1,43 +1,54 @@
-import { Box, Chip, Divider, Paper, Typography } from '@mui/material';
+import { Box, Chip, Divider, Paper, Typography } from "@mui/material";
 
 function IncomeStatstics() {
   return (
     <Box>
-      <Box sx={{ my: '20px' }}>
+      <Box sx={{ my: "20px" }}>
         <Paper
           sx={{
-            boxShadow: '0 0 10px #E1D9D1A0',
-            borderRadius: '10px',
+            boxShadow: "0 0 10px #E1D9D1A0",
+            borderRadius: "10px",
           }}
         >
           <Box
             sx={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'space-between',
-              pt: '20px',
-              px: '20px',
-              width: '100%',
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              pt: "20px",
+              px: "20px",
+              width: "100%",
             }}
           >
-            <Typography fontWeight='bolder'>Income</Typography>
+            <Typography fontWeight="bolder">Income</Typography>
             <Chip
-              label='This Month'
-              size='small'
-              sx={{ borderRadius: '5px', backgroundColor: '#F4F5F7' }}
+              label="This Month"
+              size="small"
+              sx={{ borderRadius: "5px", backgroundColor: "#F4F5F7" }}
             />
           </Box>
-          <Box sx={{ p: '10px' }}>
+          <Box sx={{ p: "10px" }}>
             <Divider />
           </Box>
-          <Box sx={{ px: '10px', py: '15px' }}>
-            <Typography>ETB 9460.00 1.5 &darr;</Typography>
+          <Box
+            sx={{
+              px: "10px",
+              py: "15px",
+              display: "flex",
+              justifyContent: "space-around",
+              alignItems: "center",
+            }}
+          >
+            <Typography fontWeight="bolder" fontSize="25px">
+              ETB 9460.00{" "}
+            </Typography>
+            <Box color="red">1.5 &darr;</Box>
           </Box>
-          <Box sx={{ px: '10px', py: '15px' }}>
-            <Typography variant='body2'>
+          <Box sx={{ px: "10px", py: "15px" }}>
+            <Typography variant="body2">
               Compared to ETB 9940 last month
             </Typography>
-            <Typography variant='body2'>
+            <Typography variant="body2">
               Last Monty income ETB 25658.00
             </Typography>
           </Box>
